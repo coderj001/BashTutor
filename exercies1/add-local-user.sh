dbb83264e296324476d83285d1dca3047461a10b
mr-zero <amirajubolchi001@gmail.com>
2020-08-26 12:26:12 +0530

    Added Comment to exercies1 & add new file in script folder

--- exercies1/add-local-user.sh
+++ exercies1/add-local-user.sh
@@ -2,6 +2,26 @@
 
 # Goal: The goal of this exercise is to create a shell script that adds users to the same Linux system as the script is executed on.
 
+# Make sure the script is being executed with superuser privileges.
+
+# Get the username (login).
+
+# Get the real name (contents for the description field).
+
+# Get the password.
+
+# Create the user with the password.
+
+# Check to see if the useradd command succeeded.
+
+# Set the password.
+
+# Check to see if the passwd command succeeded.
+
+# Force password change on first login.
+
+# Display the username, password, and the host where the user was created
+
 # Privileges check
 if [[ "${UID}" -ne 0 ]]; then
     echo "Please run with sudo or as root"
--- script/demo5.sh
+++ script/demo5.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "${0}"
+echo "Your script name: ${0}"
+
+echo "You used '$(dirname ${0})' as path to the '$(basename ${0})' script."
+
+NUMBER_OF_PARAMETERS="${#}"
+echo "You supplied ${NUMBER_OF_PARAMETERS} argument(s) on the command line."
+
+if [[ "${NUMBER_OF_PARAMETERS}" -lt 1 ]]; then
+    echo "Usage: ${0} USER_NAME [USER_NAME]..."
+    exit 1
+fi
