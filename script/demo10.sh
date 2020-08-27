1a88ad157d7f62646e38df7f9f1b80009ea3b554
mr-zero <amirajubolchi001@gmail.com>
2020-08-27 20:38:42 +0530

    Create demo files

--- script/demo10.sh
+++ script/demo10.sh
@@ -11,7 +11,23 @@ function log() {
     logger -t $0 "$MESSAGE"
 }
 
+function back_up(){
+    # This function create a backup of file. Returns non-zero status on error.
+    local FILE=${1}
+    # Make sure the file exits
+    if [[ -f "${FILE}" ]];then
+        local BACK_FILE="/var/tmp/$(basename ${FILE}).$(date +%F-%N)"
+        logger "Backing up $FILE to $BACK_FILE"
+        cp -p ${FILE} ${BACK_FILE}
+        if [[ ${?} -ne 0 ]]; then
+            echo "Backup File can't be created"
+        fi
+    else
+        # If file dosn't exits
+        return 1
+    fi
+}
 readonly VERBOSE=true
-log 'Hello!'
-log 'Your name'
-log 'Super su'
+log 'Log message 1'
+log 'Log message 2'
+back_up '/etc/passwd'
